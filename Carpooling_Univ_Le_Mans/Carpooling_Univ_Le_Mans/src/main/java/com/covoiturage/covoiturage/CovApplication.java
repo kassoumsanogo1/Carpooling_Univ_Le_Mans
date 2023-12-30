40f3381f0e14e9fc1d7e0068701c9f51b02b41c5
Kassoum SANOGO <kassoum.sanogo@outlook.com>
2023-12-31 00:24:44 +0100

    commit

---
+++ Carpooling_Univ_Le_Mans/Carpooling_Univ_Le_Mans/src/main/java/com/covoiturage/covoiturage/CovApplication.java
@@ -0,0 +1,14 @@
+package com.covoiturage.covoiturage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication
+public class CovApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CovApplication.class, args);
+		System.out.println("http://localhost:8090");
+	}
+}
---
+++ Carpooling_Univ_Le_Mans/Carpooling_Univ_Le_Mans/src/test/java/com/covoiturage/Cov/CovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covoiturage.Cov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
